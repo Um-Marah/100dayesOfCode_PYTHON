785a636d2024193441a6d4ba1defafdf10c77429
Um-Marah <51251513+Um-Marah@users.noreply.github.com>
2019-10-08 21:46:51 +0300

    Add files via upload

---
+++ Week2/day11.py
@@ -0,0 +1,21 @@
+# this program to applay the Paython Operators 2 $$ day11
+# Python Logical Operators:
+x = 5
+y = 10
+z = 5
+print(x>y or x==y)
+print(x=5 and x<y)
+print(not x==y)
+# Python Identity Operators
+print("________________________________")
+x = ["orange","banana"]
+y = ["orange","banana"]
+z = x
+print(x is not y)
+print(x is y)
+print(z is x)
+# Python Membership Operators
+print("________________________________")
+print('banana' in x)
+print('orange'not in y)
+print('Red' in y)
